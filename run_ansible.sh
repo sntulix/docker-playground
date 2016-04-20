1a496d3a7beb7d3cc1d6ffc3454f0d28ec0dca58
Takahiro Shizuki <shizu@futuregadget.com>
2016-04-20 14:36:26 +0900

    add ansible/tasks/heroku.yml.

--- run_ansible.sh
+++ run_ansible.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
 ansible-playbook -v --extra-vars "taskname=copy_ssh" ansible/playbook.yml
+ansible-playbook -v --extra-vars "taskname=heroku" ansible/playbook.yml
 ansible-playbook -v --extra-vars "taskname=finish_messages" ansible/playbook.yml
