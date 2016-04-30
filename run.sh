5539be00a742bed3afab18c308e7b1df3fe27b5a
Takahiro Shizuki <shizu@futuregadget.com>
2016-04-30 09:36:48 +0900

    run.sh, add samba ports.

--- run.sh
+++ run.sh
@@ -1,3 +1,3 @@
-#!/bin/sh
+#!/bin/bash
 SHELL_PATH=$(cd "$(dirname "$0")"; pwd)
-docker run -it --rm -v $SHELL_PATH:/mnt/dockerfile_root -v ~/:/home/host_user -p 80:80 -p 5432:5432 local/playground
+docker run -it --rm -v /$SHELL_PATH:/mnt/dockerfile_root -v /$HOME:/home/host_user -p 80:80 -p 5432:5432 -p 139:139 -p 445:445 local/playground
