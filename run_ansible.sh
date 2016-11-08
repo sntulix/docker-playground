6b9ca1ed2e8b55bf53ba865e20382eaf2cf27bdd
Takahiro Shizuki <shizuki@bauport.io>
2016-11-08 10:07:46 +0900

    run_ansible.sh, add heroku.

--- run_ansible.sh
+++ run_ansible.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
+export PATH=$PATH:~/.local/bin/
 ansible-playbook -v --extra-vars "taskname=copy_ssh" ansible/playbook.yml
-ansible-playbook -v --extra-vars "taskname=heroku" ansible/playbook.yml
+#ansible-playbook -v --extra-vars "taskname=heroku" ansible/playbook.yml
 ansible-playbook -v --extra-vars "taskname=finish_messages" ansible/playbook.yml
